324158b5f75db357275f84c9c4d42f48b4fe0077
Chris Wilson <chris@chris-wilson.co.uk>
2016-12-22 08:36:28 +0000

    drm: Add asserts to catch overflow in drm_mm_init() and drm_mm_init_scan()
    A simple assert to ensure that we don't overflow start + size when
    initialising the drm_mm, or its scanner.
    
    In future, we may want to switch to tracking the value of ranges (rather
    than size) so that we can cover the full u64, for example like resource
    tracking.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/20161222083641.2691-26-chris@chris-wilson.co.uk
    (cherry picked from commit 6259a56ba0e1c3a15954e22ea531e810944518cb)

--- drivers/gpu/drm/drm_mm.c
+++ drivers/gpu/drm/drm_mm.c
@@ -732,6 +732,8 @@ void drm_mm_init_scan(struct drm_mm *mm,
 		      unsigned alignment,
 		      unsigned long color)
 {
+	DRM_MM_BUG_ON(!size);
+
 	mm->scan_color = color;
 	mm->scan_alignment = alignment;
 	mm->scan_size = size;
@@ -767,6 +769,9 @@ void drm_mm_init_scan_with_range(struct drm_mm *mm,
 				 u64 start,
 				 u64 end)
 {
+	DRM_MM_BUG_ON(start >= end);
+	DRM_MM_BUG_ON(!size || size > end - start);
+
 	mm->scan_color = color;
 	mm->scan_alignment = alignment;
 	mm->scan_size = size;
@@ -885,6 +890,8 @@ EXPORT_SYMBOL(drm_mm_scan_remove_block);
  */
 void drm_mm_init(struct drm_mm *mm, u64 start, u64 size)
 {
+	DRM_MM_BUG_ON(start + size <= start);
+
 	INIT_LIST_HEAD(&mm->hole_stack);
 	mm->scanned_blocks = 0;
 
