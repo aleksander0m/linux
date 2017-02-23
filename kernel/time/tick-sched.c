3cffed3b30298ad90c305c0cdfe7ab318a0621e9
Philipp Zabel <p.zabel@pengutronix.de>
2017-02-23 14:49:48 +0100

    Revert "Revert "nohz: Fix collision between tick and other hrtimers""
    This reverts commit 558e8e27e73f ('Revert "nohz: Fix collision between
    tick and other hrtimers') to reinstate commit 24b91e360ef5 ("nohz: Fix
    collision between tick and other hrtimers").
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -729,6 +729,11 @@ static ktime_t tick_nohz_stop_sched_tick(struct tick_sched *ts,
 		 */
 		if (delta == 0) {
 			tick_nohz_restart(ts, now);
+			/*
+			 * Make sure next tick stop doesn't get fooled by past
+			 * clock deadline
+			 */
+			ts->next_tick = 0;
 			goto out;
 		}
 	}
@@ -771,7 +776,7 @@ static ktime_t tick_nohz_stop_sched_tick(struct tick_sched *ts,
 	tick = expires;
 
 	/* Skip reprogram of event if its not changed */
-	if (ts->tick_stopped && (expires == dev->next_event))
+	if (ts->tick_stopped && (expires == ts->next_tick))
 		goto out;
 
 	/*
@@ -791,6 +796,8 @@ static ktime_t tick_nohz_stop_sched_tick(struct tick_sched *ts,
 		trace_tick_stop(1, TICK_DEP_MASK_NONE);
 	}
 
+	ts->next_tick = tick;
+
 	/*
 	 * If the expiration time == KTIME_MAX, then we simply stop
 	 * the tick timer.
@@ -806,7 +813,10 @@ static ktime_t tick_nohz_stop_sched_tick(struct tick_sched *ts,
 	else
 		tick_program_event(tick, 1);
 out:
-	/* Update the estimated sleep length */
+	/*
+	 * Update the estimated sleep length until the next timer
+	 * (not only the tick).
+	 */
 	ts->sleep_length = ktime_sub(dev->next_event, now);
 	return tick;
 }
